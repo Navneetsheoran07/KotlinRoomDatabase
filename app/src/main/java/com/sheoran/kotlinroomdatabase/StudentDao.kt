8e977435783fba044b9f5f286070ef8ff14468b8
Navneet sheoran <navneetsheoran537@gmail.com>
2023-10-09 09:59:09 +0530

    Initial commit

---
+++ app/src/main/java/com/sheoran/kotlinroomdatabase/StudentDao.kt
@@ -0,0 +1,23 @@
+package com.sheoran.kotlinroomdatabase
+
+import androidx.room.*
+
+@Dao
+interface StudentDao {
+    @Query("SELECT * FROM student_table")
+    fun getAll(): List<Student>
+
+
+    @Query("SELECT * FROM student_table WHERE roll_no LIKE :roll LIMIT 1")
+     fun findByRoll(roll: Int): Student
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+     fun insert(student: Student)
+
+    @Delete
+     fun delete(student: Student)
+
+    @Query("DELETE FROM student_table")
+ fun deleteAll()
+
+}
